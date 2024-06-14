648a60d8b733510ad15adef3cb03955ea7ac67b5
Rahul <rontgen.rahul@gmail.com>
2024-06-14 04:21:03 +0100

    modified:   array_appear_twice.py

--- array_appear_twice.py
+++ array_appear_twice.py
@@ -1,28 +1,29 @@
-# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+""" Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-# You must implement a solution with a linear runtime complexity and use only constant extra space.
-
-
-# Example 1:
-
-# Input: nums = [2,2,1]
-# Output: 1
-# Example 2:
-
-# Input: nums = [4,1,2,1,2]
-# Output: 4
-# Example 3:
-
-# Input: nums = [1]
-# Output: 1
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 
-# def singleNumber(self, nums: List[int]) -> int:
-#         xor = 0
-#         for num in nums:
-#             xor ^= num
+Example 1:
 
-#         return xor 
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+
+"""
+
+def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        return xor 
 
 
 def single_number(nums):
